0ff3ac81f6aa8a118b319a4deb258bba4909ffa0
Hanul <hanul@hanul.me>
2021-10-25 00:27:24 +0900

    한칸 띄우기

--- src/view/Home.ts
+++ src/view/Home.ts
@@ -12,6 +12,7 @@ export default class Home implements View {
     constructor() {
         Layout.current.content.append(this.container = el(".home-view",
             new TypingText(`Facts by AI
+
 AI가 현장의 감시카메라 데이터를 통해 증언을 합니다.
 증언을 위한 케이스를 보유하고 있어야 하며, 증인은 여러 번 재시도 할 수 있습니다. 
 마음에 드는 증언이 출력되면 민팅할 수 있으며, 민팅에는 10MIX가 소모됩니다.
