9c76f71c9a74be00a5a3b71bf880b6ac9baf25c1
Sim Young Jae <hanul@hanul.me>
2023-02-02 14:16:01 +0900

    dev

--- src/klaytn/Klaytn.ts
+++ src/klaytn/Klaytn.ts
@@ -1,6 +1,6 @@
 class Klaytn {
 
-    private caver = new (window as any).Caver(new (window as any).Caver.providers.WebsocketProvider("wss://klaytn-node.klu.bs:9091"));
+    private caver = new (window as any).Caver(new (window as any).Caver.providers.WebsocketProvider("wss://klaytn04.fandom.finance/ws"));
 
     public createContract(address: string, abi: any) {
         return this.caver.contract.create(abi, address);
